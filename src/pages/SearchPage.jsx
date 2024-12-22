cc912cac267d607cfa9fa9a987b50c87231dbdc5
Aman Kumar <amankarn49@gmail.com>
2024-12-22 15:02:57 +0530

    Change frontend in search .. and page is integrated

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchNavbar from "../components/Navbar";
 import SearchBar from "./components/SearchPage/SearchBar";
 import SearchCard from "./components/SearchPage/SearchCard";
 import SearchFooter from "../components/Footer";
+
 const SearchPage = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <>
       <SearchNavbar />
-      <SearchBar />
-      <SearchCard />
+      <SearchBar setSearchResults={setSearchResults} />
+      <SearchCard centers={searchResults} />
       <SearchFooter />
     </>
   );
